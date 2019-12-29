f87612be8d3dbc947b88926e3fb9b8ebfb006ed1
adamczarnceki <adam@czarnecki.cz>
2019-12-29 17:31:28 +0100

    next page and selectror fix

--- data_miner.py
+++ data_miner.py
@@ -14,7 +14,11 @@ def get_all_pages_names(link):
     soup = BeautifulSoup(page, 'html.parser')
     h1 = soup.select('h1')[0].getText()
     print('Czytam stronę: %s' % (h1))
-    current_page = soup.select('#mw-content-text li', href=True)
+    current_page = soup.select('.mw-category-generated li', href=True)
+    is_next = soup.select('.mw-category-generated a', href=True)[-1]
+    if 'następna strona' in is_next.getText():
+        print('Jest następna strona %s' % (is_next['href']))
+        listy_kategorii.append(is_next['href'])
 
     for elem in current_page:
         if elem.select('.CategoryTreeSection') == []:
@@ -30,7 +34,11 @@ def get_all_pages_names(link):
             pass
         else:
             print('Czytam stronę: "%s"' % (h1))
-            current_page = soup.select('#mw-content-text li', href=True)
+            current_page = soup.select('.mw-category-generated li', href=True)
+            is_next = soup.select('.mw-category-generated a', href=True)[-1]
+            if 'następna strona' in is_next.getText():
+                print('Jest następna strona %s' % (is_next['href']))
+                listy_kategorii.append(is_next['href'])
             for elem in current_page:
                 if elem.select('.CategoryTreeSection') == []:
                     lista_elementow_docelowych.append(elem.getText())
@@ -40,24 +48,30 @@ def get_all_pages_names(link):
     return lista_elementow_docelowych
 
 
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    if iteration == total: 
+        print()
+
+
 def remove_not_pl(lista):
     print('Przed czyszczeniem: %s' % (len(lista)))
     polska_lista = []
     count = 0
-    five_procent = len(lista) // 100
+    printProgressBar(count, len(lista), prefix = 'Czyszczenie:', length = 50)
     for x in lista:
         if str(detect(x)) == 'pl':
             polska_lista.append(x)
         else:
             pass
         count += 1
-        if five_procent == 0:
-            pass
-        elif count % five_procent == 0:
-            print('Czyszczenie... {:>5.2f}%'.format((count / len(lista)) * 100))
+        printProgressBar(count, len(lista), prefix = 'Czyszczenie:', length = 50)
 
     return polska_lista
-
 
 
 if __name__ == '__main__':
@@ -79,13 +93,13 @@ if __name__ == '__main__':
     k = sorted(get_all_pages_names(url))
     bez_powtorzen = [k[i] for i in range(len(k)) if i == 0 or k[i] != k[i - 1]]
     if remove_latin == 0:
-        to_save = (str(bez_powtorzen))
+        to_save = bez_powtorzen
     elif remove_latin == 1:
-        to_save = (str(remove_not_pl(bez_powtorzen)))
+        to_save = remove_not_pl(bez_powtorzen)
     print('Gotowych do zapisania %s nazw' % (len(to_save)))
 
     path = os.path.dirname(os.path.abspath(__file__)) + '/'
     with open(path + file_name + '.txt', 'w', encoding='utf-8') as f:
-        f.write(to_save)    
+        f.write(str(to_save))    
     end = time.time()
     print('\nTOTAL TIME: {} min. {} sec.\n'.format(int((end - start) // 60), int((end - start) % 60)))
